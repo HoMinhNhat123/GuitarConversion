872943209b6ab3f007a1304290f6b695c91799ba
Ho Minh Nhat <hnguy119@uic.edu>
2026-08-01 17:58:20 -0500

    changing name from main.tsx to index.tsx (because I like it :)? )

---
+++ frontend/src/mainCanvas.tsx
@@ -0,0 +1,10 @@
+import './mainCanvas.css'
+
+export function MainCanvas() {
+    const contentComponent = (
+        <>
+            <h1> Hello </h1>
+        </>
+    )
+    return contentComponent
+}
